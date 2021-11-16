2548294078969352fdce3ebcf48b758551fd929a
SafwanSbr <84856781+SafwanSbr@users.noreply.github.com>
2021-11-16 11:21:03 +0600

    Create knapsack_memoization.cpp

---
+++ C++/dp/knapsack_memoization.cpp
@@ -0,0 +1,33 @@
+//Problem - https://atcoder.jp/contests/dp/tasks/dp_d
+
+#include<bits/stdc++.h>
+using namespace std;
+#define opetimize() ios_base::sync_with_stdio(0);//cin.tie(0);cout.tie(0);// Last part used to print all at the end
+#define int long long int
+
+vector < vector<int> > dp( 101 ,vector <int> (100001, -1));
+
+int knapsack( int wt[], int val[], int n, int W)
+{
+    if( W == 0 || n == 0 ) return 0;
+    if( dp[n][W] != -1) return dp[n][W];
+    if( wt[n-1] <= W ){
+        return dp[n][W] = max( val[n-1]+ knapsack( wt, val, n-1, W-wt[n-1]), knapsack( wt, val, n-1, W));
+    }
+    else return knapsack( wt, val, n-1, W);
+}
+
+signed main()
+{
+    opetimize();
+
+    int n, W;
+    cin >> n >> W;
+    int wt[n], val[n];
+    for( int i=0; i<n; i++){
+        cin >> wt[i];
+        cin >> val[i];
+    }
+
+    cout << knapsack( wt, val, n, W) << "\n";
+}
